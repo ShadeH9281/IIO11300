6cd44e73750854757425c3307a0231c8c6348e5b
ShadeH9281 <ffk7shade@gmail.com>
2016-01-27 07:22:42 +0200

    Tehtava2

---
+++ IIO11300Vktehtavat/Tehtava2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿/*
+* Copyright (C) JAMK/IT/Toni Pajukanta
+* This file is part of the IIO11300 course project.
+* Created: 26.1.2015 Modified 27.1.2016
+* Authors: Toni Pajukanta
+*/
+
+/*
+ Very early stages.
+ 
+ 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtava2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
